575ee8a912525473defa7de0c204ceb0bba6fe53
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-19 19:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
 
-  for (var i = 0; i < instruments.length; i++) {
-    array.push("${musicians[i]} plays ${instruments[i]}.");
+  for (var i = 0; i < musicians.length; i++) {
+    array.push("{musicians[i]}" + "plays" + "{instruments[i]}.");
   } return array;
 }
 
-function johnLennonFacts(){
-  
+function johnLennonFacts(array){
+  while (true) {
+    array.push("{array[i]}");
+  }
 }
